ad4f1f18b112cb80978bad28dd0ee2a496c84bac
jarthurj <jaoldaker1@gmail.com>
2020-10-12 18:30:22 -0400

    Add files via upload
    Loops practice

---
+++ loop_practice.js
@@ -0,0 +1,12 @@
+console.log("Question 1")
+for(var x = 1; x <= 20; x++){
+    if(x % 2 == 1){
+        console.log(x)
+    }
+}
+console.log("Question 2")
+var sum = 0;
+for(var x = 1; x <= 5; x++){
+    sum += x;
+    console.log('Sum:'+sum+' Num:'+x);
+}
